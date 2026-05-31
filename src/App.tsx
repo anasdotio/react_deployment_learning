ef24d46638c5ce917404bf110cb1f328ecaeb266
Anas <anasroyal385@gmail.com>
2026-05-31 16:28:17 +0530

    style: enhance App component with improved button styling and underline for header

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,28 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>This is react deployed testing react app</h1>
+      <h1
+        style={{
+          textDecoration: "underline",
+        }}
+      >
+        This is react deployed testing react app
+      </h1>
       <p>{import.meta.env.VITE_APP_NAME}</p>
-      <button>Get Started</button>
+      <button
+        style={{
+          backgroundColor: "blue",
+          maxWidth: "200px",
+          border: "none",
+          margin: "0 auto",
+          padding: "10px 20px",
+          borderRadius: "5px",
+          marginTop: "20px",
+          cursor: "pointer",
+        }}
+      >
+        Get Started
+      </button>
     </>
   );
 }
